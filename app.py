054213d44ade26340efa69246562784cbde76a6a
aqib javed 09 <maqibjaveed9@gmail.com>
2025-07-05 16:50:57 +0500

    Adding False Because Fix Response time

--- app.py
+++ app.py
@@ -177,7 +177,7 @@ if __name__ == '__main__':
     with app.app_context():
         db.create_all() 
 
-    app.run(debug=True)
+    app.run(debug=False) # I  add False because for response time 200ms
 
 
 
